1b9805d088139b3eb91914114c492d216ab5ee34
Laercio Pereira <laercio.pereira@objective.com.br>
2021-01-31 21:42:16 -0300

    add configuration to deploy on heroku

--- src/main/java/br/com/empresa/healthcheckteam/HCTConfiguration.java
+++ src/main/java/br/com/empresa/healthcheckteam/HCTConfiguration.java
@@ -1,17 +1,35 @@
 package br.com.empresa.healthcheckteam;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.flywaydb.core.Flyway;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 
 import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Configuration
 public class HCTConfiguration {
 
-    @Autowired
-    public HCTConfiguration(DataSource dataSource) {
-        Flyway.configure().baselineOnMigrate(true).dataSource(dataSource).load().migrate();
+    @Bean
+    public DataSource dataSource() throws URISyntaxException {
+        HikariConfig config = new HikariConfig();
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+        config.setJdbcUrl("jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath());
+        config.setUsername(dbUri.getUserInfo().split(":")[0]);
+        config.setPassword(dbUri.getUserInfo().split(":")[1]);
+        return new HikariDataSource(config);
+    }
+
+    @Bean
+    @DependsOn("dataSource")
+    public Flyway flyway() throws URISyntaxException {
+        Flyway load = Flyway.configure().baselineOnMigrate(true).dataSource(dataSource()).load();
+        load.migrate();
+        return load;
     }
 
 }
